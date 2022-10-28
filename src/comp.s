66f6c758ec9012d7942a381abe6fa11790473166
Wolfgang Schuster <wolfgang.schuster@vendr.com>
2022-10-28 09:34:15 -0500

    Use, agus' fix 🤦‍♂️

--- src/comp.s
+++ src/comp.s
@@ -19,23 +19,16 @@ _start:
     mov     X8, #'W'
     mov     X9, #'Q'
 
-    //adrp    X10, outstr@PAGE // load `outstr`
-    //add     X10, X10, outstr@PAGEOFF
     dadr    X10, outstr
-    //strb    W9, [X10, #1]
-    //str     X1, [X10, #1]    // load the value of X1 into the address of X10
-    //add     X10, X10, #1     // add 1 to the value in X10
-    //str     X5, [X10]    // load the value of X5 into the address of X10
-    //add     X10, X10, #1     // add 1 to the value in X10
-    //str     X5, [X10]    // load the value of X5 into the address of X10
-    //add     X10, X10, #1     // add 1 to the value in X10
-    //str     X7, [X10]    // load the value of X7 into the address of X10
-    //add     X10, X10, #1     // add 1 to the value in X10
-    //str     X6, [X10]    // load the value of X7 into the address of X10
+    strb    W1, [X10], #1 // load the value of X1 into the address of X10, then increment X10 to the next position
+    strb    W5, [X10], #1 // load the value of X5 into the address of X10, then increment X10 to the next position
+    strb    W5, [X10], #1 // load the value of X5 into the address of X10, then increment X10 to the next position
+    strb    W7, [X10], #1 // load the value of X7 into the address of X10, then increment X10 to the next position
+    strb    W6, [X10]     // load the value of X7 into the address of X10
     
     mov     X0, #1
     mov     X16, #4
-    mov     X1, X10
+    dadr    X1, outstr
     mov     X2, #6
     svc     #0x80
 
@@ -43,23 +36,5 @@ _start:
     mov     X16, #1     // system call, (1) terminates program
     svc     #0x80
 
-//outstr:     .ascii  "     \n"
-outstr:     .ascii  "WORLD\n"
-
-
-
-
-
-
-//     mov     X0, #1      // (1) is stdout
-//     adr     X1, hello   // address of `hello`
-//     mov     X2, hello_len     // length of `hello`
-//     mov     X16, #4     // system call, (4) write string
-//     svc     #0x80
-
-//     mov     X0, #0      // return code
-//     mov     X16, #1     // system call, (1) terminates program
-//     svc     #0x80
-
-// hello:      .ascii      "Hello, World!\n"
-// hello_len = . - hello
+.data
+outstr:     .ascii  "     \n"
