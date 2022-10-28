dd6c47742c0394f2a6c84a7c955d0f76dfc2c54f
Wolfgang Schuster <wolfgang.schuster@vendr.com>
2022-10-28 13:45:04 -0500

    Otter and moving memory

--- src/comp.s
+++ src/comp.s
@@ -7,6 +7,53 @@
     add     \Xn, \Xn, \name@pageoff
 .endm
 
+.macro movemem Xa, Xb
+    ldrb    W3, [\Xa], #1
+    strb    W3, [\Xb], #1
+.endm
+
+// Coping string from A to B
+_start:
+    dadr    X0, instr
+    dadr    X1, outstr
+
+    //ldrb    W3, [X0], #1
+    //strb    W3, [X1], #1
+    //ldrb    W3, [X0], #1
+    //strb    W3, [X1], #1
+    //ldrb    W3, [X0], #1
+    //strb    W3, [X1], #1
+    //ldrb    W3, [X0], #1
+    //strb    W3, [X1], #1
+    //ldrb    W3, [X0], #1
+    //strb    W3, [X1], #1
+    //ldrb    W3, [X0]
+    //strb    W3, [X1]
+    movemem X0, X1
+    movemem X0, X1
+    movemem X0, X1
+    movemem X0, X1
+    movemem X0, X1
+    movemem X0, X1
+    //mov     X3, #'\n'
+    //strb    W3, [X1]
+
+    mov     X0, #1
+    mov     X16, #4
+    dadr    X1, outstr
+    mov     X2, #6
+    svc     #0x80
+
+    mov     X0, #0      // return code
+    mov     X16, #1     // system call, (1) terminates program
+    svc     #0x80
+
+.data
+instr:      .ascii     "SUMMER"
+outstr:     .fill       7, 1, 0  // num repititions, size of repititions, what to fill with
+
+// Moving memory around
+/*
 _start:
     mov     X0, #'P'
     mov     X1, #'O'
@@ -38,3 +85,4 @@ _start:
 
 .data
 outstr:     .ascii  "     \n"
+ */
